2feaed9bad479d2545931eb0b460fafcb3833a31
ustibi <1004673248@qq.com>
2020-07-07 15:26:19 +0800

    Finish the question 2 & 3 & 4

--- hw2/hw2.R
+++ hw2/hw2.R
@@ -2,8 +2,8 @@ rm(list = ls())
 ## 1. Loading and cleaning
 # a
 library(tidyverse)
-#ca_pa <- read.csv("data/calif_penn_2011.csv", header = TRUE, sep = ",")
-ca_pa <- read_csv("data/calif_penn_2011.csv")
+ca_pa <- read.csv("data/calif_penn_2011.csv", header = TRUE, sep = ",")
+#ca_pa <- read_csv("data/calif_penn_2011.csv")
 # b
 rows <- dim(ca_pa)[1]
 columns <- dim(ca_pa)[2]
@@ -23,6 +23,16 @@ plot(ca_pa$Median_house_value, ca_pa$Built_2005_or_later,
      xlab = "median house prices",
      ylab = "percentage of houses built since 2005")
 # b
+plot(ca_pa$Median_house_value[ca_pa$STATEFP == 6],
+     ca_pa$Built_2005_or_later[ca_pa$STATEFP == 6],
+     xlab = "median house prices",
+     ylab = "percentage of houses built since 2005",
+     main = "Houses in California")
+plot(ca_pa$Median_house_value[ca_pa$STATEFP == 42],
+     ca_pa$Built_2005_or_later[ca_pa$STATEFP == 42],
+     xlab = "median house prices",
+     ylab = "percentage of houses built since 2005",
+     main = "Houses in Pennsylvania")
 
 ## 3
 # a
@@ -35,3 +45,71 @@ median(Vacancy_rate)
 # b
 plot(ca_pa$Median_house_value, ca_pa$Vacancy_rate,
      xlab = "median house prices", ylab = "vacancy rate")
+# c
+plot(ca_pa$Median_house_value[ca_pa$STATEFP == 6],
+     ca_pa$Vacancy_rate[ca_pa$STATEFP == 6],
+     xlab = "median house prices", ylab = "vacancy rate",
+     main = "Houses in California")
+plot(ca_pa$Median_house_value[ca_pa$STATEFP == 42],
+     ca_pa$Vacancy_rate[ca_pa$STATEFP == 42],
+     xlab = "median house prices", ylab = "vacancy rate",
+     main = "Houses in Pennsylvania")
+
+## 4
+# a
+acca <- c()
+for (tract in 1:nrow(ca_pa)) {
+  if (ca_pa$STATEFP[tract] == 6) {
+    if (ca_pa$COUNTYFP[tract] == 1) {
+      acca <- c(acca, tract)
+    }
+  }
+}
+accamhv <- c()
+for (tract in acca) {
+  accamhv <- c(accamhv, ca_pa[tract,10])
+}
+median(accamhv)
+# b
+median(ca_pa$Median_house_value[ca_pa$STATEFP == 6 & ca_pa$COUNTYFP == 1])
+# c
+# Alameda
+mean(ca_pa$Built_2005_or_later[ca_pa$STATEFP == 6 & ca_pa$COUNTYFP == 1])
+# Santa Clara
+mean(ca_pa$Built_2005_or_later[ca_pa$STATEFP == 6 & ca_pa$COUNTYFP == 85])
+# Allegheny
+mean(ca_pa$Built_2005_or_later[ca_pa$STATEFP == 42 & ca_pa$COUNTYFP == 3])
+# d
+# (i) the whole data
+cor(ca_pa$Median_house_value, ca_pa$Built_2005_or_later)
+# (ii) all of California
+cor(ca_pa$Median_house_value[ca_pa$STATEFP == 6],
+    ca_pa$Built_2005_or_later[ca_pa$STATEFP == 6])
+# (iii) all of Pennsylvania
+cor(ca_pa$Median_house_value[ca_pa$STATEFP == 42],
+    ca_pa$Built_2005_or_later[ca_pa$STATEFP == 42])
+# (iv) Alameda
+cor(ca_pa$Median_house_value[ca_pa$STATEFP == 6 & ca_pa$COUNTYFP == 1],
+    ca_pa$Built_2005_or_later[ca_pa$STATEFP == 6 & ca_pa$COUNTYFP == 1])
+# (v) Santa Clara
+cor(ca_pa$Median_house_value[ca_pa$STATEFP == 6 & ca_pa$COUNTYFP == 85],
+    ca_pa$Built_2005_or_later[ca_pa$STATEFP == 6 & ca_pa$COUNTYFP == 85])
+# (vi) Allegheny
+cor(ca_pa$Median_house_value[ca_pa$STATEFP == 42 & ca_pa$COUNTYFP == 3],
+    ca_pa$Built_2005_or_later[ca_pa$STATEFP == 42 & ca_pa$COUNTYFP == 3])
+# e
+#Alameda
+plot(ca_pa$Median_house_value[ca_pa$STATEFP == 6 & ca_pa$COUNTYFP == 1],
+     ca_pa$Median_household_income[ca_pa$STATEFP == 6 & ca_pa$COUNTYFP == 1],
+     xlab = "median house values", ylab = "median income",
+     main = "Houses in Alameda")
+# Santa Clara
+plot(ca_pa$Median_house_value[ca_pa$STATEFP == 6 & ca_pa$COUNTYFP == 85],
+     ca_pa$Median_household_income[ca_pa$STATEFP == 6 & ca_pa$COUNTYFP == 85],
+     xlab = "median house values", ylab = "median income",
+     main = "Houses in Santa Clara")
+# Allegheny
+plot(ca_pa$Median_house_value[ca_pa$STATEFP == 42 & ca_pa$COUNTYFP == 3],
+     ca_pa$Median_household_income[ca_pa$STATEFP == 42 & ca_pa$COUNTYFP == 3],
+     xlab = "median house values", ylab = "median income",
+     main = "Houses in Allegheny")
