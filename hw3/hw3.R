2a0e580e87479664be67bae9eca25a204aca82a3
ustibi <1004673248@qq.com>
2020-07-07 22:57:59 +0800

    Partially finish the question 4

--- hw3/hw3.R
+++ hw3/hw3.R
@@ -8,3 +8,15 @@ plot(gmp$pcgmp ~ gmp$pop, log = "x",
 curve(6611*x^(1/8), add = TRUE, col = "blue")
 curve(6611*x^(0.1), add = TRUE, col = "red")
 curve(6611*x^(0.15), add = TRUE, col = "green")
+
+# 2
+mse <- function(x, N = gmp$pop, Y = gmp$pcgmp) {
+  return(sum((Y - x[1]*N^x[2]) ^ 2) / length(N))
+}
+mse(c(6611, 0.15))
+mse(c(5000, 0.10))
+
+# 3
+nlm(mse, c(y0=6611,a=1/8))
+nlm(mse, c(y0=6611,a=0.1))
+nlm(mse, c(y0=6611,a=0.15))
